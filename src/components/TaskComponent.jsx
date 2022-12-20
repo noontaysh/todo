c2972807a1698c0efe6bf63a02990e7bc037577f
Johnny <n00ntaysh@gmail.com>
2022-12-20 17:13:14 +0200

    it seems to be done

--- src/components/TaskComponent.jsx
+++ src/components/TaskComponent.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import '../stylization/TaskComponent.css'
+import DeleteIcon from '@material-ui/icons/Delete';
 
 const TaskComponent = (props) => {
     return (
-        <div className='taskComponent'>
-            <h3>{props.content}</h3>
+        <div className='taskComponent strike'>
+            {props.markedDone ? <strike><h3>{props.content}</h3></strike> : <h3>{props.content}</h3>}
             {/*<p>{props.id}</p>*/}
             {props.markedDone ? (<input type="checkbox" onClick={() => {
                 props.unMark(props.id)
             }} checked={true}/>) : (<input type="checkbox" onClick={() => {
                 props.markDone(props.id)
             }} checked={false}/>)}
-            <button onClick={() => {props.deleteTask(props.id)}}>Del</button>
+            <DeleteIcon className='deleteIcon' onClick={() => {props.deleteTask(props.id)}} color='secondary' />
         </div>
     );
 };
